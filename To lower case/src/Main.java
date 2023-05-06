2ad0856f7f833643425da3378cb69ab5b40b302c
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-05-06 12:59:01 -0400

    To lower case - leetcode easy problem

---
+++ To lower case/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String[] args) {
+        String s = "HELLO";
+        toLowerCase(s);
+    }
+
+    public static String toLowerCase (String s) {
+        String output = s.toLowerCase();
+        return output;
+    }
+
+}
