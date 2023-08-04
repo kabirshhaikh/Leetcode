2cc135c75801ba47e7d30e33e4e0b3013140496f
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-08-04 11:17:55 -0400

    Shortest distance to a character - leetcode easy problem

---
+++ Shortest distance to a character/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main (String[] args) {
+        String s = "loveleetcode";
+        char c = 'e';
+        int[] test = shortestToChar(s, c);
+        System.out.println(Arrays.toString(test));
+    }
+
+
+
+    public static int[] shortestToChar (String s, char c) {
+        int N = s.length();
+        int[] ans = new int[N];
+        int prev = Integer.MIN_VALUE / 2;
+
+        for (int i = 0; i < N; ++i) {
+            if (s.charAt(i) == c) prev = i;
+            ans[i] = i - prev;
+        }
+
+        prev = Integer.MAX_VALUE / 2;
+        for (int i = N-1; i >= 0; --i) {
+            if (s.charAt(i) == c) prev = i;
+            ans[i] = Math.min(ans[i], prev - i);
+        }
+
+        return ans;
+    }
+
+}
