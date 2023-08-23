ac62569514e582bd293adf41900e064b06232cf6
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-08-23 10:42:01 -0400

    Number of employees who met the target - leetcode easy problem

---
+++ Number of Employees who met the target/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main (String[] args) {
+        int[] hours = {0,1,2,3,4};
+        int target = 2;
+        int test = numberOfEmployeesWhoMetTarget(hours, target);
+        System.out.println(test);
+    }
+
+    public static int numberOfEmployeesWhoMetTarget (int[] hours, int target) {
+        int count = 0;
+        for (int i=0; i< hours.length; i++) {
+            if (hours[i] >= target) {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+
+}
