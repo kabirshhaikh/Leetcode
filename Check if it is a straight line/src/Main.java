6e3f8d8b64a18db49c898cbaab7bc12dc97a1b37
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-08-31 12:06:23 -0400

    Check if it is a straight line - leetocde easy problem

---
+++ Check if it is a straight line/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main (String[] args) {
+        int[][] coordinates = {{1,2}, {2,3}};
+        boolean test = checkStraightLine(coordinates);
+        System.out.println(test);
+    }
+
+    public static boolean checkStraightLine (int[][] coordinates) {
+        if(coordinates.length<=2) {
+            return true;
+        }
+
+        int x0= coordinates[0][0];
+        int y0= coordinates[0][1];
+        int x1=coordinates[1][0];
+        int y1= coordinates[1][1];
+
+        for(int i=2;i<coordinates.length;i++){
+            int x=coordinates[i][0];
+            int y= coordinates[i][1];
+            if((y1-y0)*(x-x0)!= (y-y0)*(x1-x0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
