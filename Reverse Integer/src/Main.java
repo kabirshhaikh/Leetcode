5363e8b8555275672014646972af071ef59ec757
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-09-19 10:23:49 -0400

    Reverse Integer - leetcode medium problem

---
+++ Reverse Integer/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void main (String[] args) {
+        int x = 120;
+        int test = reverse(x);
+        System.out.println(test);
+    }
+
+
+    public static int reverse (int x) {
+        int result = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+            result = result * 10 + digit;
+            x /= 10;
+        }
+        return result;
+    }
+}
