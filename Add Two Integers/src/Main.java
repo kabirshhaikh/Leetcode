6db2ad031e129a6e6076fd00fe813d8700534e18
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-11-01 09:59:58 -0400

    Add Two Integers - leetcode easy problem

---
+++ Add Two Integers/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[] args) {
+        int num1 = -10;
+        int num2 = 4;
+        int test = sum(num1, num2);
+        System.out.println(test);
+    }
+
+    public static int sum (int num1, int num2) {
+        int answer = (num1) +  (num2);
+        return answer;
+    }
+}
