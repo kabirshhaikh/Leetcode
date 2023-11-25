ffda51dcfd00da80363225a654d3416f4e9bda97
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-11-25 11:57:10 -0500

    Sum of absolute differences in a sorted array - leetcode medium problem

---
+++ Sum of absolute differences in a sorted array/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main (String[] args) {
+        int[] nums = {2,3,5};
+        int[] test = getSumAbsoluteDifference(nums);
+        System.out.println(Arrays.toString(test));
+    }
+
+    public static int[] getSumAbsoluteDifference (int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int[] prefixSum = new int[n];
+        int[] suffixSum = new int[n];
+
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+
+        suffixSum[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixSum[i] = suffixSum[i + 1] + nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int leftSum = i * nums[i] - (i > 0 ? prefixSum[i - 1] : 0);
+            int rightSum = (i < n - 1 ? suffixSum[i + 1] : 0) - (n - 1 - i) * nums[i];
+
+            result[i] = leftSum + rightSum;
+        }
+
+        return result;
+    }
+}
