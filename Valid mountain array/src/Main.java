c2a4680ee2eedda0eccd4f0322c7d5e74c49133d
Kabir Shaikh <shaikh.kab@northeastern.edu>
2024-01-10 10:21:32 -0500

    Valid mountain array - leetcode easy problem

---
+++ Valid mountain array/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main (String[] args) {
+        int[] arr = {0,3,2,1};
+        boolean test = validMountainArray(arr);
+        System.out.println(test);
+    }
+
+
+    public static boolean validMountainArray (int[] arr) {
+        int n = arr.length;
+
+        if (n < 3) {
+            return false;
+        }
+
+        int i = 0;
+
+        while (i < n - 1 && arr[i] < arr[i + 1]) {
+            i++;
+        }
+
+        if (i == 0 || i == n - 1) {
+            return false;
+        }
+
+        while (i < n - 1 && arr[i] > arr[i + 1]) {
+            i++;
+        }
+
+        return i == n - 1;
+    }
+}
