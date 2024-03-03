08df3cc62d74a1f913508910888e4dc0e55e9b75
Kabir Shaikh <shaikh.kab@northeastern.edu>
2024-03-03 13:47:00 -0500

    Count prefix and suffix pairs 1 - leetcode easy problem

---
+++ Count prefix and suffix pairs 1/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main (String[] args) {
+        String[] words = {"a","aba","ababa","aa"};
+        int test = countPrefixSuffixPairs(words);
+        System.out.println(test);
+    }
+
+
+    public static int countPrefixSuffixPairs (String[] words) {
+        int count=0;
+        for( int i=0;i<words.length;i++ ) {
+
+            String s1= words[i];
+            for( int j=i+1;j<words.length;j++ ) {
+
+                String s2= words[j];
+                boolean ch =true;
+                for( int k=0;k<Math.min(s1.length(),s2.length());k++ ) {
+
+                    if( s1.length() > s2.length()) { ch=false;break;}
+                    if( (s1.charAt(k) != s2.charAt(k)) || (s1.charAt(s1.length()-1-k) != s2.charAt(s2.length()-1-k))) { ch=false;break;}
+                }
+
+                if( ch)  count++;
+
+            }
+        }
+
+        return count;
+    }
+
+}
