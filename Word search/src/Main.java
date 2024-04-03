407aab112f47ea064d7dfe0efe213d1ce445859e
Kabir Shaikh <shaikh.kab@northeastern.edu>
2024-04-03 10:24:17 -0400

    Word search - leetcode medium problem

---
+++ Word search/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main (String[] args) {
+        char[][] board = {{'A','B','C','E'}, {'S','F','C','S'}, {'A', 'D', 'E', 'E'}};
+        String word = "ABCCED";
+        boolean test = exist(board, word);
+        System.out.println(test);
+    }
+
+
+
+    public static boolean exist (char[][] board, String word) {
+        int r = board.length, c = board[0].length;
+
+        boolean[][] vis = new boolean[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (board[i][j] == word.charAt(0) && dfs(board, i, j, 0, vis, word)) return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean dfs(char[][] board, int i, int j, int idx, boolean[][] vis, String word) {
+        if (idx == word.length() - 1) return true;
+
+        vis[i][j] = true;
+
+        int r = board.length, c = board[0].length;
+
+        if (i - 1 >= 0 && !vis[i - 1][j] && board[i - 1][j] == word.charAt(idx + 1) && dfs(board, i - 1, j, idx + 1, vis, word))
+            return true;
+        if (i + 1 <= r - 1 && !vis[i + 1][j] && board[i + 1][j] == word.charAt(idx + 1) && dfs(board, i + 1, j, idx + 1, vis, word))
+            return true;
+        if (j - 1 >= 0 && !vis[i][j - 1] && board[i][j - 1] == word.charAt(idx + 1) && dfs(board, i, j - 1, idx + 1, vis, word))
+            return true;
+        if (j + 1 <= c - 1 && !vis[i][j + 1] && board[i][j + 1] == word.charAt(idx + 1) && dfs(board, i, j + 1, idx + 1, vis, word))
+            return true;
+
+        vis[i][j] = false;
+        return false;
+    }
+}
