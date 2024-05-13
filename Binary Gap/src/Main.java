d1be05ab8a22a8b2a0091eb7d348f6c560ad2bc5
Kabir Shaikh <shaikh.kab@northeastern.edu>
2024-05-13 12:08:45 -0500

    Binary gap - leetcode easy problem

---
+++ Binary Gap/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 22;
+        int test = binaryGap(n);
+        System.out.println(test);
+    }
+
+
+    public static int binaryGap(int N) {
+        int[] A = new int[32];
+        int t = 0;
+        for (int i = 0; i < 32; ++i)
+            if (((N >> i) & 1) != 0)
+                A[t++] = i;
+
+        int ans = 0;
+        for (int i = 0; i < t - 1; ++i)
+            ans = Math.max(ans, A[i+1] - A[i]);
+        return ans;
+    }
+}
